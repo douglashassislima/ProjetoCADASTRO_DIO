7e81e634e0c15c006c7b51bf26d2fd647bcc9766
douglashassislima <douglashenriquedeassis@gmail.com>
2021-07-28 12:58:41 -0300

    Adicionando Atributos e Metodos

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -4,10 +4,21 @@ namespace DIO.Cadastro
     public class Serie : EntidadeBase
     {
      //Atributos extras
-     private Genero Genero { get; set, }
+     private Genero Genero { get; set; }
      private string Titulo { get; set; } 
      private string Descricao { get; set; }
      private int Ano { get; set; }
-      
+
+     //Métodos
+
+     public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+     {
+         this.Id = id;
+         this.Genero = genero;
+         this.Titulo = titulo;
+         this.Titulo = titulo;
+         this.Descricao = descricao;
+         this.Ano = ano;
+     }
     }
 }
